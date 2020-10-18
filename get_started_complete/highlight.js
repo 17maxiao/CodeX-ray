bf6565c7a198f778ed33e22c86057ce0fd8f0dd5
skandasastry <adnaksskanda@gmail.com>
2020-10-17 21:05:52 -0400

    quotes work

--- get_started_complete/highlight.js
+++ get_started_complete/highlight.js
@@ -5,8 +5,9 @@ let highlightedWords = replaceWords.replace(/hackathon|love/gi, function(x) {
 
 return `<span class=${"tooltip"} id=${"highlight"} >
         <span class=${"tooltiptext"} id=${"myPopup"}>
-        This is a string! Strings are computer's ways of representing non-numerical values.
-        A couple examples of strings in Java are: ${"cat"}, ${"red"}, and ${"aslkjale"}.
+        This is a string! Strings are computer's ways of representing non-numerical values,
+        (like words).
+        A couple examples of strings in Java are: <q>cat</q>, <q>red</q>, and <q>aslkjale</q>.
         </span>
         <mark> ${x} </mark> 
         </span>`
